4fb907cf23a1b3898f9267b09d731e07dece8422
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2022-10-17 19:35:28 +0200

    Update policy struct and add mapping for holding policies

--- src/Insurance.sol
+++ src/Insurance.sol
@@ -4,6 +4,7 @@ pragma solidity =0.8.13;
 contract Insurance {
 
     struct Policy {
+        uint256 policyUniqueIdentifier;
         uint256 policyValue;
         uint256 monthlyInstallment;
         uint256 numberOfInstallments;
@@ -11,5 +12,8 @@ contract Insurance {
     }
 
     uint256 public numberOfPolicies;
+
+    mapping(uint256 => Policy) public policies;
+    
     
 }
