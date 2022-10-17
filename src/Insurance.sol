099e15d0b0e52304aa2c242e3a7dcb656e5ea376
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2022-10-17 20:03:02 +0200

    Updated calculate policy function and create policy

--- src/Insurance.sol
+++ src/Insurance.sol
@@ -3,51 +3,58 @@ pragma solidity =0.8.13;
 
 contract Insurance {
 
-    enum HOLDING_COMPANIES {
-        BINANCE,
-        AAVE,
-        COINBASE,
-        KRAKEN,
-        YEARN
-    }
-
     struct Policy {
         uint256 policyUniqueIdentifier;
         uint256 policyValue;
         uint256 monthlyInstallment;
         uint256 numberOfInstallments;
-        HOLDING_COMPANIES companyFundsInvestedIn;
+        HoldingCompany companyFundsInvestedIn;
         address owner;
     }
 
+    struct HoldingCompany {
+        uint256 id,
+        uint256 safetyRating
+    }
+
     uint256 public numberOfPolicies;
+    uint256 public numberOfHoldingCompanies;
 
     mapping(uint256 => Policy) public policies;
+    mapping(uint256 => HoldingCompany) public holdingCompanies;
+
+    mapping(address => uint256) public numberOfPoliciesPerUser;
 
     constructor() {
 
     }
 
-    //_holdingCompany is the number between 0-4 which correlates to the enum
-    function createPolicy(uint256 _cryptoValueToBeInsured, HOLDING_COMPANIES _holdingCompany) {
+    function createPolicy(uint256 _cryptoValueToBeInsured, HoldingCompany _company) {
         
-        uint256 installment = calculatePolicyInstallments();
+        uint256 installment = calculatePolicyInstallments(_cryptoValueToBeInsured, _company);
         
         policies[numberOfPolicies] = ({
             policyUniqueIdentifier = numberOfPolicies,
             policyValue = _cryptoValueToBeInsured,
             monthlyInstallment = installment,
             numberOfInstallments = 0,
-            companyFundsInvestedIn = _holdingCompany,
+            companyFundsInvestedIn = _company,
             owner = msg.sender
         });
 
+        numberOfPoliciesPerUser[msg.sender] += 1;
         numberOfPolicies++;
 
     }
 
-    function calculatePolicyInstallments() internal {
+    function calculatePolicyInstallments(uint256 _value, HoldingCompany _company) internal returns (uint256) {
+        
+        //If the user would pay the total value in 5 years of installments, what would they pay per month
+        uint256 expected5YearInstallment = _value / 60;
+        uint256 safetyAdjustment = 100 - _company.safetyRating;
+        uint256 policyInstallment = expected5YearInstallment + ((expected5YearInstallment * safetyAdjustment) / 100);
 
+        return policyInstallment;
     }
     
     
