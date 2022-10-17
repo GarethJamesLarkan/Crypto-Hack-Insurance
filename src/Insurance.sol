712224fbef59d5a12c9a709cc303bccfad21d891
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2022-10-17 19:33:47 +0200

    Added ppolicy struct

--- src/Insurance.sol
+++ src/Insurance.sol
@@ -3,6 +3,12 @@ pragma solidity =0.8.13;
 
 contract Insurance {
 
-    
+    struct Policy {
+        uint256 policyValue;
+        uint256 monthlyInstallment;
+        uint256 numberOfInstallments;
+        address owner;
+
+    }
     
 }
