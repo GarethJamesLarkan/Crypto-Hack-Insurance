db9cbaa977cc81ffb031d299ca4f282dbc3c30ae
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2022-10-17 19:48:08 +0200

    Created new policy in createPolicy

--- src/Insurance.sol
+++ src/Insurance.sol
@@ -3,11 +3,20 @@ pragma solidity =0.8.13;
 
 contract Insurance {
 
+    enum HOLDING_COMPANIES {
+        BINANCE,
+        AAVE,
+        COINBASE,
+        KRAKEN,
+        YEARN
+    }
+
     struct Policy {
         uint256 policyUniqueIdentifier;
         uint256 policyValue;
         uint256 monthlyInstallment;
         uint256 numberOfInstallments;
+        HOLDING_COMPANIES companyFundsInvestedIn;
         address owner;
     }
 
@@ -19,12 +28,26 @@ contract Insurance {
 
     }
 
-    function createPolicy() {
+    //_holdingCompany is the number between 0-4 which correlates to the enum
+    function createPolicy(uint256 _cryptoValueToBeInsured, HOLDING_COMPANIES _holdingCompany) {
+        
+        uint256 installment = calculatePolicyInstallments();
+        
+        policies[numberOfPolicies] = ({
+            policyUniqueIdentifier = numberOfPolicies,
+            policyValue = _cryptoValueToBeInsured,
+            monthlyInstallment = installment,
+            numberOfInstallments = 0,
+            companyFundsInvestedIn = _holdingCompany,
+            owner = msg.sender
+        });
+
+        numberOfPolicies++;
 
     }
 
     function calculatePolicyInstallments() internal {
-        
+
     }
     
     
