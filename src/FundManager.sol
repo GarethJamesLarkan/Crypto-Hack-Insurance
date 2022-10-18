89173575027f2971712e1ea23e4f0c7e8d2100a5
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2022-10-18 15:22:24 +0200

    Added pay policy installment function

--- src/FundManager.sol
+++ src/FundManager.sol
@@ -3,6 +3,12 @@ pragma solidity =0.8.13;
 
 contract FundManager {
 
-    
+    constructor() {
+
+    }   
+
+    function payPolicyInstallment(uint256 _policyId, uint256 _amount) public {
+
+    } 
     
 }
