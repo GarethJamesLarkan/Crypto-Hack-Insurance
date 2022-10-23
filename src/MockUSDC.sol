f0680460e274de5ac423b50cccf3a1d0851db8b2
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2022-10-23 13:57:14 +0200

    Cleaned up

--- src/MockUSDC.sol
+++ src/MockUSDC.sol
@@ -6,14 +6,10 @@ import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 contract MockUSDC is ERC20{
 
     constructor() ERC20("Mock USDC", "MUSDC") {
-
         mint(msg.sender, 1000);
-
     }
 
     function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
-    
-    
 }
