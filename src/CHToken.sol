a279ce7f72b78d58432774da3c59260591df27c5
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2022-10-23 18:03:25 +0200

    Added transfer in mint

--- src/CHToken.sol
+++ src/CHToken.sol
@@ -2,13 +2,16 @@
 pragma solidity =0.8.13;
 
 import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract CHToken is ERC20 {
     uint256 public maxSupply = 5000000;
     uint256 public maxPerWallet = 100000;
+    IERC20 usdc;
 
-    constructor() ERC20("Crypto Hack Token", "CHT") {
+    constructor(address _usdcAddress) ERC20("Crypto Hack Token", "CHT") {
         mint(msg.sender, 1000);
+        usdc = IERC20(_usdcAddress);
     }
 
     function mint(address to, uint256 amount) public {
@@ -18,6 +21,7 @@ contract CHToken is ERC20 {
             "Max per wallet reached"
         );
 
+        usdc.transferFrom(msg.sender, address(this), amount);
         _mint(to, amount);
     }
 }
